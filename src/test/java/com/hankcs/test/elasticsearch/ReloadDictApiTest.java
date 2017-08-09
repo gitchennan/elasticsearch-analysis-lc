540f40ba8fdd3a7cfd854a596d29b346366b2154
chennan <465360798@qq.com>
2017-08-09 10:03:45 +0800

    x

--- src/test/java/com/hankcs/test/elasticsearch/ReloadDictApiTest.java
+++ src/test/java/com/hankcs/test/elasticsearch/ReloadDictApiTest.java
@@ -1,40 +1,40 @@
-package com.hankcs.test.elasticsearch;
-
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.plugin.analysis.lc.LcAnalysisClientPlugin;
-import org.elasticsearch.plugin.analysis.lc.LcDictReloadAction;
-import org.elasticsearch.plugin.analysis.lc.LcDictReloadRequestBuilder;
-import org.elasticsearch.plugin.analysis.lc.LcDictReloadResponse;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.net.InetAddress;
-
-public class ReloadDictApiTest {
-
-    public static TransportClient transportClient;
-
-    @Before
-    public void initClient() throws Exception {
-        if (transportClient == null) {
-            Settings settings = Settings.builder()
-                    .put("cluster.name", "lu-search-cluster")
-                    .put("client.transport.sniff", true)
-                    .build();
-
-            transportClient = new PreBuiltTransportClient(settings, LcAnalysisClientPlugin.class);
-            transportClient.addTransportAddresses(new InetSocketTransportAddress(InetAddress.getByName("192.168.0.125"), 9300));
-        }
-    }
-
-    @Test
-    public void test_reloadDict() {
-        LcDictReloadRequestBuilder requestBuilder = new LcDictReloadRequestBuilder(transportClient, LcDictReloadAction.INSTANCE);
-        LcDictReloadResponse lcDictReloadResponse = requestBuilder.execute().actionGet();
-
-        System.out.println(lcDictReloadResponse.toString());
-    }
-}
+//package com.hankcs.test.elasticsearch;
+//
+//import org.elasticsearch.client.transport.TransportClient;
+//import org.elasticsearch.common.settings.Settings;
+//import org.elasticsearch.common.transport.InetSocketTransportAddress;
+//import org.elasticsearch.plugin.analysis.lc.LcAnalysisClientPlugin;
+//import org.elasticsearch.plugin.analysis.lc.LcDictReloadAction;
+//import org.elasticsearch.plugin.analysis.lc.LcDictReloadRequestBuilder;
+//import org.elasticsearch.plugin.analysis.lc.LcDictReloadResponse;
+//import org.elasticsearch.transport.client.PreBuiltTransportClient;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.net.InetAddress;
+//
+//public class ReloadDictApiTest {
+//
+//    public static TransportClient transportClient;
+//
+//    @Before
+//    public void initClient() throws Exception {
+//        if (transportClient == null) {
+//            Settings settings = Settings.builder()
+//                    .put("cluster.name", "lu-search-cluster")
+//                    .put("client.transport.sniff", true)
+//                    .build();
+//
+//            transportClient = new PreBuiltTransportClient(settings, LcAnalysisClientPlugin.class);
+//            transportClient.addTransportAddresses(new InetSocketTransportAddress(InetAddress.getByName("192.168.0.125"), 9300));
+//        }
+//    }
+//
+//    @Test
+//    public void test_reloadDict() {
+//        LcDictReloadRequestBuilder requestBuilder = new LcDictReloadRequestBuilder(transportClient, LcDictReloadAction.INSTANCE);
+//        LcDictReloadResponse lcDictReloadResponse = requestBuilder.execute().actionGet();
+//
+//        System.out.println(lcDictReloadResponse.toString());
+//    }
+//}
